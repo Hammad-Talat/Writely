55b4076c3f38c5f3e287997d484cbe9cecf727b7
Hammad-Talat <hammad.talat@maidencentury.com>
2025-08-07 13:25:23 +0500

    Added CRUD operations for posts likes and comments

---
+++ posts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BlogPostViewSet, CommentViewSet, LikeViewSet
+
+router = DefaultRouter()
+router.register('posts', BlogPostViewSet, basename='posts')
+router.register('comments', CommentViewSet, basename='comments')
+router.register('likes', LikeViewSet, basename='likes')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import BlogPost, Like , Comment
+from .serializers import LikeSerializer , BlogPostSerializer ,CommentSerializer
 
-# Create your views here.
+class BlogPostViewSet(viewsets.ModelViewSet):
+    queryset = BlogPost.objects.all().order_by('-created_at')
+    serializer_class = BlogPostSerializer
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by('-created_at')
+    serializer_class = CommentSerializer
+class LikeViewSet(viewsets.ModelViewSet):
+    queryset = Like.objects.all().order_by('-created_at')
+    serializer_class = LikeSerializer
